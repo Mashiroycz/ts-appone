aea2cd4434801c9090b47f369ade624ece1f8aa7
unknown <wayealpc@wayealpc.wayeal.cn>
2021-10-29 16:31:04 +0800

    eject

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: yinchengzong
+ * @Date: 2021-08-18 16:46:12
+ * @LastEditors: yinchengzong
+ * @LastEditTime: 2021-08-19 18:51:11
+ */
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Button>11</Button>
+      </div>
     </div>
   );
 }
